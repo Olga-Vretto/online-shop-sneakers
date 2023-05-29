0098b5ffef8d2d4d914a8162e446f0a581d6d829
Olga-Vretto <olgavretto@outlook.com>
2023-05-29 23:32:33 +0300

    adding card

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import headerLogo from "./assets/images/header-logo.png"
-import cart from './assets/svg/cart.svg'
-import user from "./assets/svg/user.svg"
+import headerLogo from './assets/images/header-logo.png';
+import cart from './assets/svg/cart.svg';
+import user from './assets/svg/user.svg';
+import plus from './assets/svg/plus-button.svg';
+import img1 from './assets/sneakers-img/1.png';
 
 function App() {
   return (
     <div className='wrapper'>
-      <header className="header">
-        <div className='header-left'>
-          <img className="header-logo" src={headerLogo} alt="header-logo"/>
-          <div className="header-info">
-            <h3>Super sneakers</h3>
-            <p>Магазин найкращих кросівок</p>
+      <div className='container'>
+        <header className='header'>
+          <div className='header-left'>
+            <img className='header-logo' src={headerLogo} alt='header-logo' />
+            <div className='header-info'>
+              <h3 className='header-title'>Super sneakers</h3>
+              <p>Магазин найкращих кросівок</p>
+            </div>
+          </div>
+          <ul className='header-right'>
+            <li className='header-right__li'>
+              <img src={cart} alt='cart icon' width={18} height={18} />
+              <span>750 грн.</span>
+            </li>
+            <li>
+              <img src={user} alt='user icon' width={18} height={18} />
+            </li>
+          </ul>
+        </header>
+        <div className='content'>
+          <h1 className='content-title'>Всі кросівки</h1>
+
+          <div className='card'>
+            <img className='card-image' src={img1} alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='card-bottom'>
+              <div className='card-text'>
+                <span className=' card-text__txt grey'>Ціна:</span>
+                <span className='bold'>3000 грн.</span>
+              </div>
+              <button className='card-button'>
+                <img src={plus} alt='plus' />
+              </button>
+            </div>
           </div>
         </div>
-        <ul className="header-right">
-          <li>
-          <img src={cart} alt="cart icon" width={18} height={18}/>
-            <span>750 грн.</span>
-          </li>
-          <li>
-          <img src={user} alt="user icon" width={18} height={18}/>
-          </li>
-        </ul>
-      </header>
-      <div className="content">
-        <h1>Всі кросівки</h1>
       </div>
     </div>
   );
