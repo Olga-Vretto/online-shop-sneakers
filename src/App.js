28199f501c7a91c03bb469cc61e5fc189faefcb5
Olga-Vretto <olgavretto@outlook.com>
2023-05-29 19:19:40 +0300

    add styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import headerLogo from "./assets/images/header-logo.png"
+import cart from './assets/svg/cart.svg'
+import user from "./assets/svg/user.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper'>
+      <header className="header">
+        <div className='header-left'>
+          <img className="header-logo" src={headerLogo} alt="header-logo"/>
+          <div className="header-info">
+            <h3>Super sneakers</h3>
+            <p>Магазин найкращих кросівок</p>
+          </div>
+        </div>
+        <ul className="header-right">
+          <li>
+          <img src={cart} alt="cart icon" width={18} height={18}/>
+            <span>750 грн.</span>
+          </li>
+          <li>
+          <img src={user} alt="user icon" width={18} height={18}/>
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>Всі кросівки</h1>
+      </div>
     </div>
   );
 }
